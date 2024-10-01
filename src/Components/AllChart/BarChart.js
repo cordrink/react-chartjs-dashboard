d17b331f571f903251e7058b9357c13a45840a98
cordrink <masodavictorjudicael@gmail.com>
2024-10-01 15:25:45 +0100

    Amelioration de la Bar chart

--- src/Components/AllChart/BarChart.js
+++ src/Components/AllChart/BarChart.js
@@ -1,22 +1,16 @@
 import React from "react";
-import {
-  Chart as ChartJS,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-} from "chart.js";
+import { Chart as ChartJS, registerables } from "chart.js";
 import { Bar } from "react-chartjs-2";
 import fullData from "../../context/fulldata";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement);
+ChartJS.register(...registerables);
 
 export default function BarChart() {
   const data = {
-    labels: ["jav/Fev", "Mar/Avr", "Mai/Ju", "Jui/Aout", "Sep/Oct", "Nov/Dec"],
+    labels: ["Jan/Fev", "Mar/Avr", "Mai/Ju", "Jui/Aout", "Sep/Oct", "Nov/Dec"],
     datasets: [
       {
-        axis: "y",
-        label: "Chiffres bimestriels",
+        label: "stat",
         data: fullData["2020"].chart1,
         backgroundColor: [
           "rgba(255, 99, 132, 0.8)",
@@ -40,23 +34,30 @@ export default function BarChart() {
   };
 
   const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-            min: 0,
-            max: 20,
-          },
-        },
-      ],
-    },
-    // remove box
-    legend: {
-      labels: {
-        boxWidth: 0,
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: true,
+        text: "Chiffre bimestriel"
       },
     },
+    scales: {
+      y: {
+        min: 0,
+        max: 20,
+        ticks: {
+          stepSize: 1,
+          color: "green",
+          font: {
+            family: "jetbrains Mono",
+            size: 10,
+          },
+        },
+      },
+    }
   };
 
   return (
